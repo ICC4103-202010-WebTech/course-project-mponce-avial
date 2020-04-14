d1aca0653074200ad8bd9bf21a9df5cfe0871fe3
andresvial <avial5@miuandes.cl>
2020-04-14 17:07:52 -0400

    Added validation for email and username in RegisteredUser, and added 'username' atribute to RegisteredUser

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -1,2 +1,4 @@
 class RegisteredUser < ApplicationRecord
+  validates :email_address, format: {with: /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
+  validates :username, uniqueness: true
 end
--- db/migrate/20200413234348_create_registered_users.rb
+++ db/migrate/20200413234348_create_registered_users.rb
@@ -4,6 +4,7 @@ class CreateRegisteredUsers < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :last_name
       t.integer :age
+      t.string :username
       t.string :email_address
       t.string :google_account
       t.string :password
