ddee1adb6daaad00512345c08697b2227fdf226b
andresvial <avial5@miuandes.cl>
2020-05-06 19:59:22 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :blacklists
+
+      resources :events do
+        resources :event_guests
+        resources :event_images
+        resources :event_pdfs
+        resources :event_videos
+        resources :comments, shallow: true do
+          resources :replies, shallow: true
+        end
+      end
+
+      resources :messages
+    end
+  end
 end
