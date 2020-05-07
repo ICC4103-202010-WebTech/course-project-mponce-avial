4a91dcef2f7c6ff76ed7dd444b7021b73b431ce7
mp11b <maponce@miuandes.cl>
2020-05-07 12:58:49 -0400

    User Profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_registereduser
+
+  private
+
+  def set_registereduser
+    @@registered_user = RegisteredUser.first
+  end
 end
