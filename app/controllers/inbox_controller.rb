2bff9954a83116bae10e7f562a96c8088c147844
andresvial <avial5@miuandes.cl>
2020-05-09 21:18:17 -0400

    Added inbox

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -4,7 +4,7 @@ class InboxController < ApplicationController
   # GET /inbox
   # GET /inbox.json
   def index
-    @inbox = Inbox.all
+    #@inbox = Inbox.all
   end
 
   # GET /inbox/1
@@ -15,7 +15,7 @@ class InboxController < ApplicationController
 
   # GET /inbox/new
   def new
-    @inbox = Inbox.new
+    #@inbox = Inbox.new
   end
 
   # GET /inbox/1/edit
@@ -25,51 +25,51 @@ class InboxController < ApplicationController
   # POST /inbox
   # POST /inbox.json
   def create
-    @inbox = Inbox.new(inbox_params)
+    #@inbox = Inbox.new(inbox_params)
 
-    respond_to do |format|
-      if @inbox.save
-        format.html { redirect_to @inbox, notice: 'Inbox was successfully created.' }
-        format.json { render :show, status: :created, location: @inbox }
-      else
-        format.html { render :new }
-        format.json { render json: @inbox.errors, status: :unprocessable_entity }
-      end
-    end
+    #respond_to do |format|
+    # if @inbox.save
+    #   format.html { redirect_to @inbox, notice: 'Inbox was successfully created.' }
+    #   format.json { render :show, status: :created, location: @inbox }
+    # else
+    #   format.html { render :new }
+    #   format.json { render json: @inbox.errors, status: :unprocessable_entity }
+    # end
+    #end
   end
 
   # PATCH/PUT /inbox/1
   # PATCH/PUT /inbox/1.json
   def update
-    respond_to do |format|
-      if @inbox.update(inbox_params)
-        format.html { redirect_to @inbox, notice: 'Inbox was successfully updated.' }
-        format.json { render :show, status: :ok, location: @inbox }
-      else
-        format.html { render :edit }
-        format.json { render json: @inbox.errors, status: :unprocessable_entity }
-      end
-    end
+  # respond_to do |format|
+  #   if @inbox.update(inbox_params)
+  #     format.html { redirect_to @inbox, notice: 'Inbox was successfully updated.' }
+  #     format.json { render :show, status: :ok, location: @inbox }
+  #   else
+  #     format.html { render :edit }
+  #     format.json { render json: @inbox.errors, status: :unprocessable_entity }
+  #   end
+  # end
   end
 
   # DELETE /inbox/1
   # DELETE /inbox/1.json
   def destroy
-    @inbox.destroy
-    respond_to do |format|
-      format.html { redirect_to inbox_index_url, notice: 'Inbox was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  # @inbox.destroy
+  # respond_to do |format|
+  #   format.html { redirect_to inbox_index_url, notice: 'Inbox was successfully destroyed.' }
+  #   format.json { head :no_content }
+  # end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_inbox
-      @inbox = Inbox.find(params[:id])
+      #@inbox = Inbox.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def inbox_params
-      params.fetch(:inbox, {})
+      #params.fetch(:inbox, {})
     end
 end
