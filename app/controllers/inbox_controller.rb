1752b932ab725698dc38c90467d48db2968ea2ef
mp11b <maponce@miuandes.cl>
2020-05-09 20:03:25 -0400

    Inbox

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -10,6 +10,7 @@ class InboxController < ApplicationController
   # GET /inbox/1
   # GET /inbox/1.json
   def show
+    @inbox_message_recived = Message.where(destination_user_id: 3)
   end
 
   # GET /inbox/new
