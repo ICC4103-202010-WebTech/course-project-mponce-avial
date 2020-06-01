018208525dead4e882a1026259b99e4ec38c97d3
andresvial <avial5@miuandes.cl>
2020-05-31 21:38:43 -0400

    Added search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,13 @@ class PagesController < ApplicationController
   end
 
   def search
+    if params[:q].blank?
+      @results = {}
+    else
+      @parameter = params[:q].downcase
+      @users = RegisteredUser.all.where("lower(name) LIKE :search OR lower(last_name) LIKE :search OR lower(username) LIKE :search ", search: "%#{@parameter}%")
+      @organizations = Organization.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
+      @events = Event.all.where("lower(title) LIKE :search OR lower(description) LIKE :search", search: "%#{@parameter}%")
+    end
   end
 end
