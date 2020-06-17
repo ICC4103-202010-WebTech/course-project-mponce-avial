b0722b3630445e93b65730603b5293b9bfe53e57
Andres <avial5@miuandes.cl>
2020-06-16 22:20:23 -0400

    Added deletion on users

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -1,14 +1,14 @@
 class RegisteredUser < ApplicationRecord
   validates :email_address, format: {with: /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
   validates :username, uniqueness: true
-  has_many :events, dependent: :destroy
+  has_many :events, dependent: :destroy, foreign_key: "event_creator_id"
   has_one :user_profile, dependent: :destroy
-  has_one :blacklist, dependent: :destroy
-  has_many :organization_admins, dependent: :destroy
+  has_one :blacklist, dependent: :destroy, foreign_key: "blacklist_owner_id"
+  has_many :organization_admins, dependent: :destroy, foreign_key: "admin_id"
   has_many :comments, dependent: :destroy
   has_many :replies, dependent: :destroy
   has_many :event_guests, dependent: :destroy
-  has_many :messages, dependent: :destroy
+  has_many :messages, dependent: :destroy, foreign_key: "sender_user_id"
   has_many :organization_members, dependent: :destroy
   has_many :system_administrators, dependent: :destroy
 end
