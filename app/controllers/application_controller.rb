65176845d3a41c9e9812e5aa954a10c06a75b3e5
Andres <avial5@miuandes.cl>
2020-06-20 13:09:57 -0400

    Login attemp V.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :location, :age, :username])
   end
 end
