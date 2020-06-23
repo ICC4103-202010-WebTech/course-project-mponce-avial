02faf64d3a48219f26986e205da699763b6b9ae2
Andres <avial5@miuandes.cl>
2020-06-22 22:22:19 -0400

    Now if a user is deleted and the organization doesnt have any more admins, the org will also be deleted

--- app/models/organization_admin.rb
+++ app/models/organization_admin.rb
@@ -1,4 +1,13 @@
 class OrganizationAdmin < ApplicationRecord
   belongs_to :admin, :class_name => 'RegisteredUser', :foreign_key => 'admin_id'
   belongs_to :organization
+
+  after_destroy :check_for_org_destruction
+
+  private
+    def check_for_org_destruction
+      if self.organization.organization_admins.count == 0
+        self.organization.destroy
+      end
+    end
 end
