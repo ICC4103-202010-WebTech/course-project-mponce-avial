ce8663dc642eabe6ba04f8ba9411be3f3b1b92da
Web Tech <maponce@miuandes.cl>
2020-06-23 21:35:48 -0400

    Fixed minor erros

--- app/controllers/blacklists_controller.rb
+++ app/controllers/blacklists_controller.rb
@@ -15,14 +15,14 @@ class BlacklistsController < ApplicationController
   # GET /blacklists/new
   def new
     @reported_type = params[:reported_type]
-    if @reported_type = "event"
+    @r_type = @reported_type
+    if @r_type = "event"
       @reported = Event.find(params[:reported_id])
-    elsif @reported_type = "organization"
+    elsif @r_type = "organization"
       @reported = Organization.find(params[:reported_id])
-    elsif @reported_type = "user"
+    elsif @r_type = "user"
       @reported = RegisteredUser.find(params[:reported_id])
     end
-    @blacklist = Blacklist.new
   end
 
   # GET /blacklists/1/edit
